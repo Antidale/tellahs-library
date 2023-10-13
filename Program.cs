9f875f2d364f2b818631a4e7989ccf6813bd2c2a
antidale <antidale@gmail.com>
2023-10-13 05:16:04 -0700

    adding serilog

--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
 ﻿using DSharpPlus;
 using DSharpPlus.SlashCommands;
 using tellahs_library.Commands;
+using Serilog;
+using Microsoft.Extensions.Logging;
 
 var token = Environment.GetEnvironmentVariable("DiscordBotToken");
+
+Log.Logger = new LoggerConfiguration()
+                    .WriteTo.Console()
+                    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 10)
+                    .CreateLogger();
+
+var logFactory = new LoggerFactory().AddSerilog();
 
 var discord = new DiscordClient(new DiscordConfiguration
 {
     Token = token,
     TokenType = TokenType.Bot,
-    Intents = DiscordIntents.AllUnprivileged
+    Intents = DiscordIntents.AllUnprivileged,
+    LoggerFactory = logFactory
 });
 
 var slash = discord.UseSlashCommands();
