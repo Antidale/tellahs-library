6563bd778bba28285084ed0e2e478b943a663da3
antidale <antidale@gmail.com>
2023-10-26 15:47:18 -0700

    Add enum file, remove commented out code

--- Commands/Tournament.cs
+++ Commands/Tournament.cs
@@ -1,43 +1,10 @@
 ﻿using DSharpPlus.SlashCommands;
-using DSharpPlus.SlashCommands.Attributes;
 using tellahs_library.Enums;
 
 namespace tellahs_library.Commands
 {
     public class Tournament : ApplicationCommandModule
     {
-        //[SlashCommand("ScheduleRace", "Creates an event")]
-        //[GuildOnly]
-        //public async Task SetRace(InteractionContext ctx,
-        //    [Option("raceName", "who is racing who")] string raceName,
-        //    [Option("Time", "")] string timeString,
-        //    [Option("day", "")] int day,
-        //    //month
-        //    [Choice("Jan", 1)]
-            
-        //    [Option("month", "")] int month = 0,
-            
-        //    [Option("year", "")] int year = 0
-        //)
-        //{
-            
-        //}
-
-        
-        [SlashCommand("StartPoisonPicking", "Initiates the flag draft")]
-        public async Task StartDraftAsync(InteractionContext ctx,
-            [Option("player1", "player one")] string player1,
-            [Option("player2", "player two")] string player2,
-            [Option("player3", "player three (optional)")] string player3 = "",
-            [Option("player4", "player four (optional)")] string player4 = "",
-            [Option("player5", "player five (optional)")] string player5 = ""
-            )
-        {
-            await ctx.CreateResponseAsync("okay");
-            var playerString = string.Join("|", player1, player2, player3, player4, player5);
-
-        }
-
         [SlashCommand("SelectPB2JFlagset", "stuff")]
         public async Task SelectPB2JFlagsetAsync(InteractionContext ctx,
             [Option("VetoChoice", "Flagset that's vetoed")]Pb2jFlagetChoices pb2JFlagsetChoice)
---
+++ Enums/Pb2jFlagetChoices.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus.SlashCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tellahs_library.Enums
+{
+    public enum Pb2jFlagetChoices
+    {
+        [ChoiceName("None")]
+        None,
+        [ChoiceName("Ladder PB2J")]
+        Ladder,
+        [ChoiceName("Hop 'Til You Shop")]
+        HopTillYouShop,
+        [ChoiceName("Pro")]
+        Pro
+    }
+}
