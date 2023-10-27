7f25201e8e9ce61c99d926e246176952df2aba13
Antidale <antidale@gmail.com>
2023-10-27 15:00:58 -0700

    Adding initial flagset chooser for pb2j tourney

---
+++ Enums/Pb2jFlagsetChoices.cs
@@ -0,0 +1,16 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace tellahs_library.Enums
+{
+    public enum Pb2jFlagsetChoices
+    {
+        [ChoiceName("None")]
+        None,
+        [ChoiceName("Ladder PB2J")]
+        Ladder,
+        [ChoiceName("Hop 'Til You Shop")]
+        HopTillYouShop,
+        [ChoiceName("Pro")]
+        Pro
+    }
+}
---
+++ Services/RandomService.cs
@@ -0,0 +1,10 @@
+﻿namespace tellahs_library.Services
+{
+    public class RandomService
+    {
+        private readonly Random _random = new();
+
+        public int Next() => _random.Next();
+        public int Next(int count) => _random.Next(count);
+    }
+}
