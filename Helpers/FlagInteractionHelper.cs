eb0ccabff51b85a56ab3e34c25a803423ee9e262
antidale <antidale@gmail.com>
2023-12-28 11:55:17 -0800

    update to 4.4.6, underground => Underworld

--- Helpers/FlagInteractionHelper.cs
+++ Helpers/FlagInteractionHelper.cs
@@ -11,9 +11,9 @@ namespace tellahs_library.Helpers
 
             return flagInteractionChoice switch
             {
-                FlagInteractionChoices.Kmiab_UndergroundAccess => embedBuilder.WithTitle("Kmiab vs Underground Access")
+                FlagInteractionChoices.Kmiab_UnderworldAccess => embedBuilder.WithTitle("Kmiab vs Underworld Access")
                     .WithDescription(
-@"When `Kmiab` is enabled, the monster boxes in the following areas can be part of the logical path to the Underground:
+@"When `Kmiab` is enabled, the monster boxes in the following areas can be part of the logical path to the Underworld:
 * Eblan Castle
 * Eblan Cave
 * Giant of Bab-il
@@ -21,7 +21,7 @@ namespace tellahs_library.Helpers
 * Tower of Zot
 * Upper Bab-il
 
-If either `Kmoon` or `Kunsafe` have also been enabled, the nine monster boxes in the Lunar Subterrane can also gate your Underground access
+If either `Kmoon` or `Kunsafe` have also been enabled, the nine monster boxes in the Lunar Subterrane can also gate your Underworld access
 ")
                     
 
@@ -36,12 +36,15 @@ When both are enabled, the anchoring changes that are part of `Chero` do not app
                     .Build(),
 
                 FlagInteractionChoices.ForceMagma_DMist => embedBuilder.WithTitle("Kforce:magma vs D.Mist")
-                    .WithDescription("When both `Kforce:magma` and `Knofree` are enabled, D.Mist can gate logical underground access at any location that does not require falling down the pitfall and which follows underground access modifications from other flags. As an example, this means that a D.Mist at the vanilla Rubicant position can either have the Magma key, or a different KI that gates the Magma key.")
+                    .WithDescription(
+@"When both `Kforce:magma` and `Knofree` are enabled, D.Mist can have (or gate) logical Underworld access at any location that does not require falling down the pitfall and which follows Underworld access modifications from other flags. 
+
+As an example, this means that a D.Mist at the vanilla Rubicant position can either have the Magma key, or a different KI that gates the Magma key.")
                     .Build(),
                 
                 FlagInteractionChoices.BVanilla_OdinSpot => embedBuilder.WithTitle("Bvanilla vs Odin's spot")
                     .WithDescription(
-@"When `Ksummon` and `Bvanilla` are enabled, the Odin spot can gate your logical underground access. This is not intended behavior, but do not expect this to change before 5.0.
+@"When `Ksummon` and `Bvanilla` are enabled, the Odin spot can gate your logical Underworld access. This is not intended behavior, but do not expect this to change before 5.0.
 
 See [this](<https://discord.com/channels/411615349579186178/411616251836628993/942203585486274570>) explanation in #bug-reports in the Free Enterprise Workshop for the official explanation")
                     .Build(),
