efee2b640dc985f0fa93715ea3df583689a706a8
antidale <antidale@gmail.com>
2024-06-06 23:52:45 -0700

    Remove Random service, use Random.Shared instead

--- Commands/FlagsetChooser.cs
+++ Commands/FlagsetChooser.cs
@@ -1,27 +1,18 @@
 ﻿using DSharpPlus.SlashCommands;
 using tellahs_library.Enums;
-using tellahs_library.Services;
 using static tellahs_library.Helpers.Pb2jFlagsetHelper;
 
 namespace tellahs_library.Commands
 {
     public class FlagsetChooser : ApplicationCommandModule
     {
-        public RandomService? RandomService { get; set; }
-
         [SlashCommand("SelectPB2JFlagset", "Selects one non-vetoed PB2J flagset at Random")]
         public async Task SelectPB2JFlagsetAsync(
             InteractionContext ctx,
             [Option("VetoChoice", "Flagset that's vetoed")] Pb2jFlagsetChoices pb2JFlagsetChoice
         )
         {
-            if (RandomService == null)
-            {
-                await ctx.CreateResponseAsync("I'm sorry, something's wrong");
-                return;
-            }
-
-            var (flagsetDetails, selectedFlagset) = GetFlagset(pb2JFlagsetChoice, RandomService);
+            var (flagsetDetails, selectedFlagset) = GetFlagset(pb2JFlagsetChoice);
 
             await ctx.CreateResponseAsync(
 $@"Vetoed Set: {pb2JFlagsetChoice.GetName()}
