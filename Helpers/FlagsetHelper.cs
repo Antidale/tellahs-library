d728e1252cd0346321db99250aca9c0c1adf4d93
antidale <antidale@tellah.life>
2024-07-30 17:37:31 -0700

    Adding recall suggested-flagset command

---
+++ Helpers/FlagsetHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace tellahs_library.Helpers
+{
+    public static class FlagsetHelper
+    {
+        public static string GetSuggestedFlagsets()
+        {
+            return
+"""
+A few starting flagset suggestions:
+
+    [Casual](<https://ff4fe.com/make/bBAYAAAAAAAAAAAAABEUAAAAAAAAAAAAAAgACRAH-AACC>) - Find the Crystal, beat up Zeromus. You can find great gear in treasure chests or for sale in shops.
+    [Supermarket Sweep](<https://ff4fe.com/make/bBAYAAAAAAAAAAEBGBkQAAAAAAAAAABAAAgBGRAEwAAAC>) - found in the Ladder Collection. Free shops with the best weapons and armor available to outfit all your favorite characters. Objectives might have you chasing all over the game, but your Key Items are limited to the Main set of locations
+    [Lali-Ho Redux](<https://ff4fe.com/make/bBAYAIK3iAAAAACBSXsgoAAAAAAAAADCEAhABJAFwIYIiIA>) - a more beginner friendly version of the Lali-Ho Swiss flags (spring 2021 tournament). Found in the Ladder Collection
+
+For runners who've played a seed or two, or watched a few races, here are a some suggested flagsets for some additional challenge or some specific practice:
+
+    [Intermediate](<https://ff4fe.com/make/bBAYAIK3iAAAAACBSXsgoAAAAAAAAADCEAhABJAFwIYIiIA>) - Shops get tuned down, but characters get J abilities. Still plenty of power available in treasure boxes.
+    [Ladder Standard](<https://ff4fe.com/make/bBAYAAAAAAAAAAHByHqgAAAAAAAAAADCAAgABJAF4AoAC>) - A really good basic flagset once you've had a few runs to get a feel for the randomizer. `Cnofree` makes your early game a little harder. Found in the Ladder Collection.
+    [Adamant Cup Group Stage](<https://ff4fe.com/make/bBAYAAAAAAAAAAHCCHqgAAAAAAACAADSCAhQBJAFwAgAC>) - Very favorable treasure, and you still can get Adamant Armor, but only 4 characters in your party and restricting Cecil bumps up the difficulty a bit. Found in the Adamant Cup presets.
+    [Falcon %](<https://ff4fe.com/make/bBAYAwAMAAAAAAAAQRaIAAAAAAABAABCAAQAJJAF4AYAC>) - Great practice for Hook routes. You'll get enough stuff to get through, but odds are good it won't be easy. Found in the Ladder Collection.
+""";
+        }
+    }
+}
