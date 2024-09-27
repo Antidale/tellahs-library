a2b9e397b952b5f857d932d861768f7fccb45946
Antidale <antidale@tellah.life>
2024-09-26 19:33:34 -0700

    Adding twitch_name as a required registration field

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ var apiKey = Environment.GetEnvironmentVariable("FE_Info_Api_Key");
 #if DEBUG
 token = Environment.GetEnvironmentVariable("TestBotToken");
 apiKey = "test";
-httpClient = new HttpClient { BaseAddress = new Uri("https://localhost:5001/api/") };
+httpClient = new HttpClient { BaseAddress = new Uri("http://localhost:5000/api/") };
 #endif
 
 if (token is null) { throw new ArgumentNullException(nameof(token)); }
