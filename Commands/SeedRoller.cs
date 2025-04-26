3c77a6b6158a5f1629b581dc15cf2c2c33a60f51
Antidale <antidale@tellah.life>
2025-04-26 15:22:32 -0700

    Finish first pass of presets, fix not bailing early on error

--- Commands/SeedRoller.cs
+++ Commands/SeedRoller.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel;
 using DSharpPlus.Commands.ArgumentModifiers;
-using DSharpPlus.Commands.Processors.SlashCommands.ArgumentModifiers;
 using tellahs_library.DTOs;
 using tellahs_library.Enums;
 using tellahs_library.Helpers;
@@ -42,18 +41,7 @@ public class SeedRoller(FeGenerationHttpClient client)
             seed = seed
         };
 
-        var response = await SeedRollerHelper.RollSeedAsync(client, generateRequest, selectedSite);
-        if (!string.IsNullOrWhiteSpace(response.Error))
-        {
-            await ctx.EditResponseAsync($"Error rolling seed: {response.Error}");
-            await ctx.LogErrorAsync(response.Error);
-        }
-        else
-        {
-            await ctx.EditResponseAsync(response.ToEmbedList(flags));
-            await ctx.LogUsageAsync();
-        }
-
+        await RollSeedAndPresentResultsAsync(ctx, generateRequest, selectedSite);
     }
 
     [Command("preset")]
@@ -61,24 +49,43 @@ public class SeedRoller(FeGenerationHttpClient client)
     public async Task RollPresetAsync(SlashCommandContext ctx,
         [Parameter("choice")]
         [Description("The preset to use for rolling a seed")]
-        [SlashAutoCompleteProvider<PresetsChoiceProvider>]
-        string choice,
+        FePresetChoices choice,
         [Description("seed value to use for rolling. if used, use 5 or more characters")]
         [MinMaxLength(0, 10)]
         string? seed = null
         )
     {
 
-        await ctx.RespondAsync($"{choice} selected");
-        return;
+        await ctx.DeferResponseAsync();
 
-        // if (seed is not null && seed.Length < 5)
-        // {
-        //     seed = null;
-        // }
+        if (seed is not null && seed.Length < 5)
+        {
+            seed = null;
+        }
 
-        //TODO: get flags and site based on preset choice, then follow same path as above
+        var presetDetails = PresetHelper.GetPresetDetails(choice);
+        var generateRequest = new GenerateRequest
+        {
+            flags = presetDetails.Flagset,
+            seed = seed
+        };
 
+        await RollSeedAndPresentResultsAsync(ctx, generateRequest, presetDetails.Api);
+    }
+
+    async Task RollSeedAndPresentResultsAsync(CommandContext ctx, GenerateRequest generateRequest, FeHostedApi api)
+    {
+        var response = await SeedRollerHelper.RollSeedAsync(client, generateRequest, api);
+        if (!string.IsNullOrWhiteSpace(response.Error))
+        {
+            await ctx.EditResponseAsync($"Error rolling seed: {response.Error}");
+            await ctx.LogErrorAsync(response.Error);
+        }
+        else
+        {
+            await ctx.EditResponseAsync(response.ToEmbedList(generateRequest.flags));
+            await ctx.LogUsageAsync();
+        }
     }
 
 }
