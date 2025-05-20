035ddc83a1773408ac1aec9318a364a461fcf2a1
Antidale <antidale@tellah.life>
2025-05-19 18:12:57 -0700

    Working version of racetime race creation

---
+++ RacingCommands/Enums/AfcFlagset.cs
@@ -0,0 +1,13 @@
+using DSharpPlus.Commands.Processors.SlashCommands.ArgumentModifiers;
+
+namespace tellahs_library.RacingCommands.Enums;
+
+public enum AfcFlagset
+{
+    [ChoiceDisplayName("Adamant Cup Experience")]
+    Ace,
+    [ChoiceDisplayName("Firebomb Fiesta")]
+    Fbf,
+    [ChoiceDisplayName("Zemus Zone: Anthology")]
+    Zza,
+}
--- RacingCommands/Requests/CreateRace.cs
+++ RacingCommands/Requests/CreateRace.cs
@@ -1,12 +1,13 @@
-using System;
+using System.Text;
 
 namespace tellahs_library.RacingCommands.Requests;
 
 public class CreateRace
 {
-    public string Goal { get; set; } = string.Empty;
+    public string? Goal { get; set; } = string.Empty;
+    public string? CustomGoal { get; set; } = null;
     public bool TeamRace { get; set; } = false;
-    public bool Invidational { get; set; } = false;
+    public bool Invitational { get; set; } = false;
     public bool Unlisted { get; set; } = false;
     public bool Partitionable { get; set; } = false;
     public bool HideEntrants { get; set; } = false;
@@ -25,4 +26,7 @@ public class CreateRace
     public bool AllowMidraceChat { get; set; } = true;
     public bool AllowNonEntrantChat { get; set; } = false;
     public int ChatMessageDelay { get; set; } = 0;
+
+    public StringContent ToStringContent() =>
+        new($"goal={Goal}&custom_goal={CustomGoal}&ranked={Ranked}&info_user={InfoUser}&start_delay={StartDelay}&time_limit={TimeLimit}&streaming_required={StreamingRequired}&auto_start={AutoStart}&allow_comments={AllowComments}&hide_comments={HideComments}&allow_prerace_chat={AllowPreraceChat}&allow_midrace_chat={AllowMidraceChat}&allow_non_entrant_chat={AllowNonEntrantChat}&chat_message_delay={ChatMessageDelay}&recordable=True", Encoding.UTF8, "application/x-www-form-urlencoded");
 }
