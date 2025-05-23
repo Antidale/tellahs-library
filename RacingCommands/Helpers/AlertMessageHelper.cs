4a816153e5d9c53800d4171ff8538e0d1794ad47
Antidale <antidale@tellah.life>
2025-05-22 19:59:07 -0700

    refactor to make the main command easier to read

---
+++ RacingCommands/Helpers/AlertMessageHelper.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace tellahs_library.RacingCommands.Helpers;
+
+public static class AlertMessageHelper
+{
+    public static DiscordMessageBuilder CreateAlertMessage(SlashCommandContext ctx, string description, string raceUrl, bool shouldPing)
+    {
+        //Don't have the workshop's ping-to-race role ID to just hard code so we have to pull it from the context
+        var pingRole = ctx.Guild?.Roles.FirstOrDefault(x => x.Value.Name.Equals("ping-to-race", StringComparison.InvariantCultureIgnoreCase)).Value;
+
+        if (pingRole is null) { shouldPing = false; }
+
+        var raceDescriptionText = shouldPing
+            ? $"{pingRole!.Mention}\n### {description}"
+            : description;
+
+        var messageBuilder = new DiscordMessageBuilder()
+            .EnableV2Components()
+            .AddTextDisplayComponent(new DiscordTextDisplayComponent(raceDescriptionText))
+            .AddSeparatorComponent(new DiscordSeparatorComponent(divider: true, spacing: DiscordSeparatorSpacing.Small))
+            .AddTextDisplayComponent(new DiscordTextDisplayComponent(@$"### Race Info:
+URL: {raceUrl}
+Created by: {ctx.Member!.DisplayName}"));
+
+        if (shouldPing)
+        {
+            messageBuilder.WithAllowedMention(new RoleMention(pingRole!));
+        }
+
+        return messageBuilder;
+    }
+}
