a5afee19fbf3abae0bbc386786b69442ed8c01cc
Antidale <antidale@tellah.life>
2025-05-22 19:09:00 -0700

    Adding message to #race-alerts channel with a ping for ping-to-race

--- Constants/ChannelIds.cs
+++ Constants/ChannelIds.cs
@@ -5,5 +5,7 @@
     {
         public const ulong BotLogsChannelId = 1180243688652288010;
         public const ulong BotUsageChannelId = 1189733038192594974;
+        public const ulong AntiServerRaceAlertsId = 985334647049297972;
+        public const ulong WorkshopRaceAlertsId = 438195383185702912;
     }
 }
