c69202f0e4b84386e63d082f626eefe8689d2835
Antidale <antidale@tellah.life>
2025-05-26 12:38:08 -0700

    Slightly better flags comparison. still has subflag ordering issues

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -5,4 +5,5 @@ namespace tellahs_library.Extensions;
 public static class StringExtensions
 {
     public static bool HasContent(this string value) => !string.IsNullOrWhiteSpace(value);
+    public static string SortFlags(this string value) => string.Join(' ', value.Split(' ').OrderBy(x => x)).Trim();
 }
