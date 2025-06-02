5a699f4b0b7e2c5bb58ef4dab35dd9d2c665eb06
Antidale <antidale@tellah.life>
2025-06-02 14:06:24 -0700

    Add attribute and move embed colors to attribute on enum values

--- RacingCommands/Enums/RtggGoal.cs
+++ RacingCommands/Enums/RtggGoal.cs
@@ -1,18 +1,24 @@
 using System.ComponentModel;
 using DSharpPlus.Commands.Processors.SlashCommands.ArgumentModifiers;
+using tellahs_library.Attributes;
 
 namespace tellahs_library.RacingCommands.Enums;
 
 public enum RtggGoal
 {
     [ChoiceDisplayName("Beat Zeromus")]
+    [DiscordColor(0xCE1141)]
     BeatZeromus,
     [ChoiceDisplayName("Complete Objectives")]
+    [DiscordColor(0xff6baa)]
     WinGame,
     [ChoiceDisplayName("Door Rando - Season -1")]
+    [DiscordColor(0x006BB6)]
     DoorRando,
     [ChoiceDisplayName("5.0 alpha/beta")]
+    [DiscordColor(0x7332a8)]
     Alpha,
     [ChoiceDisplayName("Entrance Rando - Season -1")]
+    [DiscordColor(0x674736)]
     EntranceRando,
 }
