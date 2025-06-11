dbd820e5a6cdb50d4226aafd3b85baab04c5f801
Antidale <antidale@tellah.life>
2025-06-11 16:50:11 -0700

    should allow non-entrant chat for races

--- RacingCommands/Requests/CreateRace.cs
+++ RacingCommands/Requests/CreateRace.cs
@@ -24,7 +24,7 @@ public class CreateRace
     public bool HideComments { get; set; } = true;
     public bool AllowPreraceChat { get; set; } = true;
     public bool AllowMidraceChat { get; set; } = true;
-    public bool AllowNonEntrantChat { get; set; } = false;
+    public bool AllowNonEntrantChat { get; set; } = true;
     public int ChatMessageDelay { get; set; } = 0;
 
     public StringContent ToStringContent() =>
