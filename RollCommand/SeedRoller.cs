008e46cead7fcc00c2102d3fbf2fae15beb40489
Antidale <antidale@tellah.life>
2025-06-17 09:34:53 -0700

    Make seed rolling use v2 components, not embeds

--- RollCommand/SeedRoller.cs
+++ RollCommand/SeedRoller.cs
@@ -56,22 +56,19 @@ public class SeedRoller(FeGenerationHttpClient client, FeInfoHttpClient feInfoHt
         FePresetChoices choice,
         [Description("seed value to use for rolling. if used, use 5 or more characters")]
         [MinMaxLength(5, 10)]
-        string? seed = null,
-        [Description("For your eyes only")]
-        [Parameter("just_me")]
-        bool justMe = false
-        )
+        string? seed = null
+    )
     {
-        await ctx.DeferResponseAsync(ephemeral: justMe);
+        await ctx.DeferResponseAsync();
 
-        var presetDetails = PresetHelper.GetPresetDetails(choice);
+        var (Api, Flagset) = PresetHelper.GetPresetDetails(choice);
         var generateRequest = new GenerateRequest
         {
-            flags = presetDetails.Flagset,
+            flags = Flagset,
             seed = seed
         };
 
-        await RollSeedAndPresentResultsAsync(ctx, generateRequest, presetDetails.Api);
+        await RollSeedAndPresentResultsAsync(ctx, generateRequest, Api);
     }
 
     async Task RollSeedAndPresentResultsAsync(CommandContext ctx, GenerateRequest generateRequest, FeHostedApi api)
@@ -81,15 +78,19 @@ public class SeedRoller(FeGenerationHttpClient client, FeInfoHttpClient feInfoHt
         {
             await ctx.EditResponseAsync($"Error rolling seed: {response.Error}");
             await ctx.LogErrorAsync(response.Error);
+            return;
         }
-        else
+
+        var messages = response.ToMessageBuilders(generateRequest.flags, generateRequest.seed);
+        await ctx.EditResponseAsync(messages.First());
+        messages.RemoveAt(0);
+        foreach (var message in messages)
         {
-            await ctx.EditResponseAsync(response.ToEmbedList(generateRequest.flags, generateRequest.seed));
-            var seedInfo = new LogSeedRoled(ctx.User.Id, new SeedInformation(response.Version, response.Seed, response.Flags, response.Verification, response.Url));
-
-            await SeedRollerHelper.LogRolledSeedAsync(feInfoHttpClient, seedInfo);
+            await ctx.FollowupAsync(message);
         }
 
+        var seedInfo = new LogSeedRoled(ctx.User.Id, new SeedInformation(response.Version, response.Seed, response.Flags, response.Verification, response.Url));
 
+        await SeedRollerHelper.LogRolledSeedAsync(feInfoHttpClient, seedInfo);
     }
 }
