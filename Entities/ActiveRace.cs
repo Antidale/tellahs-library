0b001c744deb02d54c3d46d659820ba514b18201
Antidale <antidale@tellah.life>
2025-09-04 22:37:42 -0700

    further wip

--- Entities/ActiveRace.cs
+++ Entities/ActiveRace.cs
@@ -4,9 +4,9 @@ namespace tellahs_library.Entities;
 
 public class ActiveRace
 {
-    public string MessageIdString { get; set; }
-    public string Url { get; set; }
-    public string Description { get; set; }
-    public string Goal { get; set; }
+    public string MessageIdString { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Goal { get; set; } = string.Empty;
     public int EntrantCount { get; set; }
 }
