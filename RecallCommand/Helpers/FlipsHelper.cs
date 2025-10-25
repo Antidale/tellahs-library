adf749a39f71e4fbe0d7e4886a9b783453fa2ab2
Antidale <antidale@tellah.life>
2025-10-25 12:53:47 -0700

    some better handling and reporting of some issues

--- RecallCommand/Helpers/FlipsHelper.cs
+++ RecallCommand/Helpers/FlipsHelper.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Data;
+using Microsoft.Extensions.Logging;
 
 namespace tellahs_library.RecallCommand.Helpers;
 
 public class FlipsHelper
 {
-    public static async Task<string> CreateBpsPatchAsync(string fileName)
+    public static async Task<string> CreateBpsPatchAsync(string fileName, CommandContext ctx)
     {
         var flipsPath = Environment.GetEnvironmentVariable("TL_FLIPS_PATH");
         var romPath = Environment.GetEnvironmentVariable("TL_FE_ROM_PATH");
@@ -17,11 +19,17 @@ public class FlipsHelper
         flips.StartInfo.UseShellExecute = false;
         flips.StartInfo.CreateNoWindow = true;
         flips.StartInfo.RedirectStandardOutput = true;
+        flips.StartInfo.RedirectStandardError = true;
 
         flips.Start();
 
         //consider redirecting error output, too, and capturing that, assuming flips would give error output.
         var output = await flips.StandardOutput.ReadToEndAsync();
+        var error = await flips.StandardError.ReadToEndAsync();
+        if (!output.Contains("successfully!"))
+        {
+            throw new InvalidOperationException($"output: {output}\r\nerror: {error}");
+        }
         await flips.WaitForExitAsync();
         return outputPatchName;
     }
