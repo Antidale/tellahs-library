c9aba3e947c618fde7de30429a47673cce7e2ba9
Antidale <antidale@tellah.life>
2026-03-27 11:02:31 -0700

    Adding eegp specific goal for 1v1 race, removing door/entrance rando goals

--- RacingCommands/Enums/RtggGoal.cs
+++ RacingCommands/Enums/RtggGoal.cs
@@ -11,13 +11,7 @@ public enum RtggGoal
     [ChoiceDisplayName("Complete Objectives")]
     [DiscordColor(0xff6baa)]
     WinGame,
-    [ChoiceDisplayName("Door Rando - Season -1")]
-    [DiscordColor(0x006BB6)]
-    DoorRando,
     [ChoiceDisplayName("5.0 alpha/beta")]
     [DiscordColor(0x7332a8)]
     Alpha,
-    [ChoiceDisplayName("Entrance Rando - Season -1")]
-    [DiscordColor(0x674736)]
-    EntranceRando,
 }
